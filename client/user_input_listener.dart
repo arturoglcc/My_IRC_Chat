082e56e3c90d5dbd11fe5040d0ce4eb5f66bcdc1
Arturo <arturo_guerrero@ciencias.unam.mx>
2024-09-12 13:54:06 -0600

    isFirstMessage fixed 1

--- client/user_input_listener.dart
+++ client/user_input_listener.dart
@@ -1,26 +1,34 @@
 import 'dart:io';
 import 'client.dart';
+import 'client_messages.dart';
+import 'writer.dart';
 
 class UserInputListener {
   final Socket socket;
   bool isFirstMessage = true;
   final Client client;
+  late Writer writer;
+  late ClientMessages clientMessages;
 
-  UserInputListener(this.socket, this.client);
+  UserInputListener(this.socket, this.client) {
+    this.writer = Writer(socket);
+    this.clientMessages = ClientMessages(writer);
+  }
 
   void startListening() {
     print('Bienvenido al servidor, escribe tu nombre de usuario: ');
     stdin.listen((data) {
       String message = String.fromCharCodes(data).trim();
-
       if (message.isNotEmpty) {
         if (isFirstMessage == true) {
           client.set_username(message);
-        }
-        try {
-          socket.write(message);
+          isFirstMessage = false;
+        } else {
+          try {
+            clientMessages.processMessage(message);
         } catch (e) {
           print('Failed to send message: $e');
+          }
         }
       }
     });
